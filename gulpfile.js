df0aec4e684808625ab3c492553510d9a6eb3713
YuraSadilenko <yura.sadilenko.it@gmail.com>
2019-01-18 11:40:58 +0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
 var server = require('gulp-server-livereload');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+ 
+gulp.task('autoprefix', function() {
+  return gulp.src("css/main.css")
+  .pipe(autoprefixer({
+      browsers: ['last 10 version'],
+      cascade: true
+  }))
+});
  
 gulp.task('sass', function () {
   gulp.src('scss/main.scss')
